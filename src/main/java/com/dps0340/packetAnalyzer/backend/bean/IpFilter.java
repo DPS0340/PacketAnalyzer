dbef7d50427fb6284830fab0c8d1ac43a92c1525
jiho lee <jiholee@kpu.ac.kr>
2020-02-28 13:48:44 +0900

    ipfilter

--- src/main/java/com/dps0340/packetAnalyzer/backend/bean/IpFilter.java
+++ src/main/java/com/dps0340/packetAnalyzer/backend/bean/IpFilter.java
@@ -1,4 +1,25 @@
 package com.dps0340.packetAnalyzer.backend.bean;
 
+import org.apache.catalina.filters.RemoteAddrFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
 public class IpFilter {
+
+    @Bean
+    public FilterRegistrationBean remoteAddressFilter() {
+
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        RemoteAddrFilter filter = new RemoteAddrFilter();
+
+        filter.setAllow("127.0.0.1");
+        filter.setAllow("localhost");
+        filter.setAllow("0:0:0:0:0:0:0:1");
+        filter.setDenyStatus(404);
+
+        filterRegistrationBean.setFilter(filter);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
 }
