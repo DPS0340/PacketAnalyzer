e69113fae03205dcee2fd3ac9ff7550fccf586c2
jiho lee <jiholee@kpu.ac.kr>
2020-02-28 11:52:39 +0900

    IndexController

---
+++ src/main/java/com/dps0340/packetAnalyzer/backend/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.dps0340.packetAnalyzer.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String Index() {
+        return "index";
+    }
+
+}
