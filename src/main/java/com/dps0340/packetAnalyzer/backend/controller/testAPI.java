9d7cabbe8a20ea808bd06f87cc522af6be751593
jiho lee <jiholee@kpu.ac.kr>
2020-02-28 15:06:14 +0900

    TestAPI Writed

--- src/main/java/com/dps0340/packetAnalyzer/backend/controller/testAPI.java
+++ src/main/java/com/dps0340/packetAnalyzer/backend/controller/testAPI.java
@@ -1,4 +1,15 @@
 package com.dps0340.packetAnalyzer.backend.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class testAPI {
+    @RequestMapping(value = "/testapi", method = RequestMethod.GET)
+    @ResponseBody
+    public String getData() {
+        return "Hello world!";
+    }
 }
