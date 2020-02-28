bbbf13549d48885e45c77e52395bdbab3baae6f1
jiho lee <jiholee@kpu.ac.kr>
2020-02-28 15:05:55 +0900

    Frontend(React) Writed

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,3 +1,31 @@
 'use strict';
 
 const element = React.createElement;
+
+
+class Index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {apiResponse: ""};
+    }
+
+    state = {
+        apiResponse: null
+    };
+
+    callAPI() {
+        fetch("http://localhost:8888/testapi")
+            .then(res => res.text())
+            .then(res => this.setState({apiResponse: res}));
+    }
+
+    componentDidMount() {
+        this.callAPI();
+    }
+
+    render() {
+        return <h1>Result is: {this.state.apiResponse}</h1>;
+    }
+}
+
+ReactDOM.render(<Index />, document.getElementById("main"));
