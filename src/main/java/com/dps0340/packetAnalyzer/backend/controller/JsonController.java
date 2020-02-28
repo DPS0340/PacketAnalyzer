0c2596eb900e6e41ed068b9c5d4d9e3ea7ace7fd
jiho lee <jiholee@kpu.ac.kr>
2020-02-28 12:52:39 +0900

    temp return value on /getdata

--- src/main/java/com/dps0340/packetAnalyzer/backend/controller/JsonController.java
+++ src/main/java/com/dps0340/packetAnalyzer/backend/controller/JsonController.java
@@ -1,5 +1,6 @@
 package com.dps0340.packetAnalyzer.backend.controller;
 
+import org.json.JSONObject;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +10,6 @@ import java.util.Map;
 public class JsonController {
     @RequestMapping(value = "/getdata", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
     public String getData(@RequestBody Map<String, String> body) {
-        
+        return new JSONObject(body).toString();
     }
 }
